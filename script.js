b9550417eab21e1042e5e309663426ca3140eada
milton-web <milton.tuti@chasacademy.se>
2024-11-01 09:36:53 +0100

    "Lagt in en funktion som tar bort ett item i listan"

--- script.js
+++ script.js
@@ -14,6 +14,16 @@ function (markAsComplete) {
 
 }
 
-function (deleteToDo) {
-    
+function removeItem(deleteToDo) {
+    if (deleteToDo > 1 || deleteToDo > todoList.lenght) {
+        console.log("Invalid item to remove, please enter an item that exists");
+        return;
+        
+    } else {
+        let removedItem = todoList.splice(deleteToDo - 1, 1);
+        console.log(`Removed: "${removedItem[0]}"`);
+        showToDo();
+        
+        
+    }
 }
