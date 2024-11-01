05464d97733de9e500b81a4c4a608ac045c7efa6
DavidTheMe <71494143+DavidTheMe@users.noreply.github.com>
2024-11-01 10:25:10 +0100

    addToDo created and task class created

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
-function (mainMenu) {
+class task {
+    constructor(taskID, taskDescription, isComplete) {
+        this.taskID = taskID
+        this.taskDescription = taskDescription;
+        this.isComplete = isComplete;
+    }
+}
+
+function mainMenu() {
 
 }
 
-function (addToDo) {
+function addToDo() {
+    // Generate a unique ID
+    let newID;
+    do {
+        newID = Math.random();
+    } while (toDoList.some(task => task.taskID === newID)); // Check if ID is in use
+
+    //Create and return new task
+    let newTask = new task(newID, prompt(), false);
+    return newTask;
+}
+
+function showToDo() {
 
 }
 
-function (showToDo) {
+function markAsComplete() {
 
 }
 
-function (markAsComplete) {
+function deleteToDo() {
 
-}
-
-function (deleteToDo) {
-    
 }
