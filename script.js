af049159ec9b890a61dbf7ce26bda891b79804bf
filipwretner <filip.wretner@chasacademy.se>
2024-11-01 09:35:22 +0100

    Added initial showToDo func

--- script.js
+++ script.js
@@ -7,7 +7,15 @@ function (addToDo) {
 }
 
 function (showToDo) {
-
+    if (todolist.length === 0) {
+        console.log(`Just nu finns det inga uppgifter att visa`);
+        alert(`Just nu finns det inga uppgifter att visa`);
+    } else {
+        console.log(`Alla uppgifter:`);
+        alert(`Alla uppgifter:`);
+        todolist.forEach(todo => console.log(`Visa ID: Visa beskrivning: Visa status:`));
+        todolist.forEach(todo => alert(`Visa ID: Visa beskrivning: Visa status:`));
+    }
 }
 
 function (markAsComplete) {
