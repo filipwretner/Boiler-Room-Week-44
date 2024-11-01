0b0a97d78b39bc6e610834b242024dbf998425f8
Asplund-chasacademy <andreas.asplund@chasacademy.se>
2024-11-01 10:28:13 +0100

    första markascomplete

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
-function (mainMenu) {
+function mainMenu() {
+
+}
+// Global array för att lagra uppgifter
+
+
+function addToD() {
 
 }
 
-function (addToDo) {
+function showToDo() {
 
 }
+let tasks = [];
 
-function (showToDo) {
+function markAsComplete() {
+    let toDoListId = prompt('Ange ID för uppgiften du vill markera som klar:');
+    // Be användaren om id för den uppgift som ska markeras som klar
+    // Konvertera input till ett nummer om det behövs
+    toDoListId = parseInt(toDoListId);
 
+    // Hitta uppgiften med det angivna id:t
+    let taskFound = false;
+    for (let i = 0; i < tasks.length; i++) {
+        if (tasks[i].id === toDoListId) {
+            // Markera uppgiften som klar
+            tasks[i].completed = true;
+            alert(`Uppgiften med ID ${toDoListId} har markerats som klar.`);
+            taskFound = true;
+            break;
+        }
+    }
+
+    // Om ingen uppgift hittades med det id:t
+    if (!taskFound) {
+        alert(`Ingen uppgift med ID ${toDoListId} hittades.`);
+    }
 }
 
-function (markAsComplete) {
 
-}
 
-function (deleteToDo) {
+function deleteToDo() {
     
 }
